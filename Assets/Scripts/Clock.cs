7490db20cf9c9c70cc47aa3b7f8adaddde212108
Daniel <70732249+sunghogo@users.noreply.github.com>
2025-04-02 13:55:52 -0400

    3.5 Waking Up

---
+++ Assets/Scripts/Clock.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Clock : MonoBehaviour
+{
+    [SerializeField] private Transform _hoursPivot, _minutesPivot, _secondsPivot;
+
+    // Start is called before the first frame update
+    void Awake() {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/HourIndicatorSpawner.cs
+++ Assets/Scripts/HourIndicatorSpawner.cs
@@ -2,18 +2,18 @@ using UnityEngine;
 
 public class HourIndicatorSpawner : MonoBehaviour
 {
-    public GameObject HourIndicatorPrefab;
-    public float radius = 4f;
+    [SerializeField] private GameObject _HourIndicatorPrefab;
+    [SerializeField] private float _radius = 4f;
 
     // Start is called before the first frame update
     void Start()
     {
         for (int i = 0; i < 12; i ++) {
-            float x = Mathf.Cos(2 * Mathf.PI * i / 12) * radius;
-            float y = Mathf.Sin(2 * Mathf.PI * i / 12) * radius;
+            float x = Mathf.Cos(2 * Mathf.PI * i / 12) * _radius;
+            float y = Mathf.Sin(2 * Mathf.PI * i / 12) * _radius;
             Vector3 position = new Vector3(x, y, transform.position.z);
             Quaternion rotation = Quaternion.Euler(0f, 0f, 90 + (i * 360f / 12));
-            Instantiate(HourIndicatorPrefab, position, rotation);
+            Instantiate(_HourIndicatorPrefab, position, rotation);
         }
     }
 }
